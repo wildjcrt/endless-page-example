462ea2fd19e25aa568a9a1b31eb4521fcb20ca31
wildjcrt <wildjcrt@yahoo.com.tw>
2012-12-06 22:43:29 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EndlessPageExample::Application.config.secret_token = 'a3f493b8ad195ec0ececfb5a51f1729bfadc094049fc58243498aa8dc5622cc9751c986b4038859b7626952ab6b93e781f0ba01632ca102ed5a6634bce783c24'
