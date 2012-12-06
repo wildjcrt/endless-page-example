8e5ab082539827b374bb5fe3f78a7348432689e8
wildjcrt <wildjcrt@yahoo.com.tw>
2012-12-06 23:41:37 +0800

    posts/index.html.erb, add paginate and partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.page(params[:page]).per(1)
 
     respond_to do |format|
       format.html # index.html.erb
