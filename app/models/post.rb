2c2af697bb0b2b3257329519291d2a9d9ef1e81e
wildjcrt <wildjcrt@yahoo.com.tw>
2012-12-06 23:28:06 +0800

    annotate -p before

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ActiveRecord::Base
   attr_accessible :content, :title
 end
